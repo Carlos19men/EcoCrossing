de53a328d0aef3c8a2c7132ac61040a8dfb38f80
Carlos19men <carlosalcides190304@gmail.com>
2024-06-01 11:31:44 -0400

    repositorio de maria

--- Juego/src/main/java/com/mycompany/juego/Juego.java
+++ Juego/src/main/java/com/mycompany/juego/Juego.java
@@ -11,6 +11,6 @@ package com.mycompany.juego;
 public class Juego {
 
     public static void main(String[] args) {
-        System.out.println("Hello World! adios amor me voy de esta vez para siempre");
+        System.out.println("Hello World! soy maria");
     }
 }
