6f22a5c100ba3b516696516f60ed0c74a5905d88
Carlos19men <carlosalcides190304@gmail.com>
2024-06-01 01:46:09 -0400

    Commit de samuel

--- Juego/src/main/java/com/mycompany/juego/Juego.java
+++ Juego/src/main/java/com/mycompany/juego/Juego.java
@@ -11,6 +11,6 @@ package com.mycompany.juego;
 public class Juego {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World!  soy samuel");
     }
 }
